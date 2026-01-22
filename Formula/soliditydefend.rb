2092f0593047961983e04b4990bea197fde4308a
dehvCurtis <dehvonc@gmail.com>
2026-01-22 16:54:31 -0700

    chore: update Homebrew formula to v1.10.5

--- Formula/soliditydefend.rb
+++ Formula/soliditydefend.rb
@@ -2,21 +2,21 @@
 class Soliditydefend < Formula
   desc "High-performance static analysis security tool for Solidity smart contracts"
   homepage "https://github.com/BlockSecOps/SolidityDefend"
-  version "1.10.4"
+  version "1.10.5"
   license "MIT OR Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.4/soliditydefend-v1.10.4-darwin-arm64.tar.gz"
-      sha256 "6b2a6d951ddad1ab32b1fc17c81c19a9d223e7aa691c46c4e5edd1e4fe80ecef"
+      url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.5/soliditydefend-v1.10.5-darwin-arm64.tar.gz"
+      sha256 "07e458f4268cc8aec7c95a6a9e6a68c2b0a5927f5e8fcc6e8e0c04ec6574fe98"
     else
-      url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.4/soliditydefend-v1.10.4-darwin-x86_64.tar.gz"
+      url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.5/soliditydefend-v1.10.5-darwin-x86_64.tar.gz"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_X86_64"
     end
   end
 
   on_linux do
-    url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.4/soliditydefend-v1.10.4-linux-x86_64.tar.gz"
+    url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.5/soliditydefend-v1.10.5-linux-x86_64.tar.gz"
     sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX"
   end
 
@@ -42,7 +42,7 @@ class Soliditydefend < Formula
 
   test do
     # Test that the binary runs and shows version
-    assert_match "soliditydefend 1.10.4", shell_output("#{bin}/soliditydefend --version")
+    assert_match "soliditydefend 1.10.5", shell_output("#{bin}/soliditydefend --version")
 
     # Test help command
     assert_match "USAGE:", shell_output("#{bin}/soliditydefend --help")
